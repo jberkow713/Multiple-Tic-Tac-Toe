e59926d6ca713c541894b2c8e7254be612c4d94c
jesse berkowitz <jberkow@brandeis.edu>
2021-01-05 16:34:58 -0500

    function to find company names from 13_F

---
+++ CompanyName.py
@@ -0,0 +1,241 @@
+A = {
+  "total": {
+    "value": 10000,
+    "relation": "gte"
+  },
+  "query": {
+    "from": 1,
+    "size": 3
+  },
+  "filings": [
+    {
+      "id": "f446f11ba994940080446f6a8ac840a3",
+      "accessionNo": "0001104659-21-000864",
+      "cik": "1177132",
+      "ticker": "",
+      "companyName": "RMR Advisors LLC",
+      "companyNameLong": "RMR Advisors LLC (Filer)",
+      "formType": "13F-HR",
+      "description": "Form 13F-HR - Quarterly report filed by institutional managers, Holdings",
+      "filedAt": "2021-01-05T15:39:28-05:00",
+      "linkToTxt": "https://www.sec.gov/Archives/edgar/data/1177132/000110465921000864/0001104659-21-000864.txt",
+      "linkToHtml": "https://www.sec.gov/Archives/edgar/data/1177132/000110465921000864/0001104659-21-000864-index.htm",
+      "linkToXbrl": "",
+      "linkToFilingDetails": "https://www.sec.gov/Archives/edgar/data/1177132/000110465921000864/xslForm13F_X01/primary_doc.xml",
+      "entities": [
+        {
+          "companyName": "RMR Advisors LLC (Filer)",
+          "cik": "1177132",
+          "irsNo": "760702617",
+          "stateOfIncorporation": "MD",
+          "fiscalYearEnd": "0930",
+          "type": "13F-HR",
+          "act": "34",
+          "fileNo": "028-10878",
+          "filmNo": "21505225"
+        }
+      ],
+      "documentFormatFiles": [
+        {
+          "sequence": "1",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1177132/000110465921000864/xslForm13F_X01/primary_doc.xml",
+          "type": "13F-HR",
+          "size": " "
+        },
+        {
+          "sequence": "1",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1177132/000110465921000864/primary_doc.xml",
+          "type": "13F-HR",
+          "size": "1910"
+        },
+        {
+          "sequence": "2",
+          "description": "INFORMATION TABLE",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1177132/000110465921000864/xslForm13F_X01/a21-1587_2informationtable.xml",
+          "type": "INFORMATION TABLE",
+          "size": " "
+        },
+        {
+          "sequence": "2",
+          "description": "INFORMATION TABLE",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1177132/000110465921000864/a21-1587_2informationtable.xml",
+          "type": "INFORMATION TABLE",
+          "size": "586"
+        },
+        {
+          "sequence": " ",
+          "description": "Complete submission text file",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1177132/000110465921000864/0001104659-21-000864.txt",
+          "type": " ",
+          "size": "4047"
+        }
+      ],
+      "dataFiles": [],
+      "seriesAndClassesContractsInformation": [],
+      "periodOfReport": "2020-12-31"
+    },
+    {
+      "id": "70de4e14dd88063eb66265eb82c6f2b5",
+      "accessionNo": "0001010911-21-000006",
+      "cik": "1010911",
+      "ticker": "",
+      "companyName": "S&T BANK/PA",
+      "companyNameLong": "S&T BANK/PA (Filer)",
+      "formType": "13F-HR/A",
+      "description": "Form 13F-HR/A - Quarterly report filed by institutional managers, Holdings: [Amend]",
+      "filedAt": "2021-01-05T14:33:40-05:00",
+      "linkToTxt": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000006/0001010911-21-000006.txt",
+      "linkToHtml": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000006/0001010911-21-000006-index.htm",
+      "linkToXbrl": "",
+      "linkToFilingDetails": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000006/xslForm13F_X01/primary_doc.xml",
+      "entities": [
+        {
+          "companyName": "S&T BANK/PA (Filer)",
+          "cik": "1010911",
+          "irsNo": "250776600",
+          "stateOfIncorporation": "PA",
+          "fiscalYearEnd": "1231",
+          "type": "13F-HR/A",
+          "act": "34",
+          "fileNo": "028-06460",
+          "filmNo": "21504933"
+        }
+      ],
+      "documentFormatFiles": [
+        {
+          "sequence": "1",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000006/xslForm13F_X01/primary_doc.xml",
+          "type": "13F-HR/A",
+          "size": " "
+        },
+        {
+          "sequence": "1",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000006/primary_doc.xml",
+          "type": "13F-HR/A",
+          "size": "2440"
+        },
+        {
+          "sequence": "2",
+          "description": "INFORMATION TABLE",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000006/xslForm13F_X01/information_table.xml",
+          "type": "INFORMATION TABLE",
+          "size": " "
+        },
+        {
+          "sequence": "2",
+          "description": "INFORMATION TABLE",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000006/information_table.xml",
+          "type": "INFORMATION TABLE",
+          "size": "70924"
+        },
+        {
+          "sequence": " ",
+          "description": "Complete submission text file",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000006/0001010911-21-000006.txt",
+          "type": " ",
+          "size": "74597"
+        }
+      ],
+      "dataFiles": [],
+      "seriesAndClassesContractsInformation": [],
+      "periodOfReport": "2019-09-30"
+    },
+    {
+      "id": "0c517d8c1cd8064d256562c084878393",
+      "accessionNo": "0001010911-21-000005",
+      "cik": "1010911",
+      "ticker": "",
+      "companyName": "S&T BANK/PA",
+      "companyNameLong": "S&T BANK/PA (Filer)",
+      "formType": "13F-HR/A",
+      "description": "Form 13F-HR/A - Quarterly report filed by institutional managers, Holdings: [Amend]",
+      "filedAt": "2021-01-05T14:31:45-05:00",
+      "linkToTxt": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000005/0001010911-21-000005.txt",
+      "linkToHtml": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000005/0001010911-21-000005-index.htm",
+      "linkToXbrl": "",
+      "linkToFilingDetails": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000005/xslForm13F_X01/primary_doc.xml",
+      "entities": [
+        {
+          "companyName": "S&T BANK/PA (Filer)",
+          "cik": "1010911",
+          "irsNo": "250776600",
+          "stateOfIncorporation": "PA",
+          "fiscalYearEnd": "1231",
+          "type": "13F-HR/A",
+          "act": "34",
+          "fileNo": "028-06460",
+          "filmNo": "21504924"
+        }
+      ],
+      "documentFormatFiles": [
+        {
+          "sequence": "1",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000005/xslForm13F_X01/primary_doc.xml",
+          "type": "13F-HR/A",
+          "size": " "
+        },
+        {
+          "sequence": "1",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000005/primary_doc.xml",
+          "type": "13F-HR/A",
+          "size": "2437"
+        },
+        {
+          "sequence": "2",
+          "description": "INFORMATION TABLE",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000005/xslForm13F_X01/information_table.xml",
+          "type": "INFORMATION TABLE",
+          "size": " "
+        },
+        {
+          "sequence": "2",
+          "description": "INFORMATION TABLE",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000005/information_table.xml",
+          "type": "INFORMATION TABLE",
+          "size": "1156"
+        },
+        {
+          "sequence": " ",
+          "description": "Complete submission text file",
+          "documentUrl": "https://www.sec.gov/Archives/edgar/data/1010911/000101091121000005/0001010911-21-000005.txt",
+          "type": " ",
+          "size": "4826"
+        }
+      ],
+      "dataFiles": [],
+      "seriesAndClassesContractsInformation": [],
+      "periodOfReport": "2019-09-30"
+    }
+  ]
+}
+
+
+
+#goal is to create function that parses this and returns a list of company names:
+#The get_13F function returns a dictionary
+
+# We can then parse the dictionary with this function, to return all of the names of the companies
+# Which the venture company has invested in 
+
+
+def Find_Company_Name(Dict):
+  Company_Names = []
+  for k,v in Dict.items():
+    if k == 'filings':
+      for x in v:
+        for k,v in x.items():
+          if k == 'entities':
+            for x in v:
+              for k,v in x.items():
+                if k == 'companyName':
+                  Company_Names.append(v)
+
+  Amended_Company_Names = []
+  for x in Company_Names:
+    y = x.split(' ')
+    y = y[:-1]
+    x = str(' '.join(y))
+    Amended_Company_Names.append(x)
+  return(Amended_Company_Names)
+
+print(Find_Company_Name(A))    
