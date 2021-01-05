c0f80f879a553842902c09224a2bf00f1d0190de
jesse berkowitz <jberkow@brandeis.edu>
2021-01-05 11:41:01 -0500

    basic idea

--- ResumeHelper.py
+++ ResumeHelper.py
@@ -1,86 +1,72 @@
-'If you were in charge of a project or initiative from start to finish, skip “led” and instead try:'
+#The idea is to get keywords used in resume analysis when it comes to how employers filter 
+#What we want to do , is for a given job description, we want to create a model that can figure out 
+# the main theme or themese of the given article
+
+#So we can use the same type of analysis in NLPModels, to take in a given description, and have it filter
+# into the below categories:
+
+#Leadership, Creation, Efficiency, Growth_Boosting, Process_Improving, Management, 
+#Client_Based, Consulting, Research Analysis, Writing/Communication, 
+#Management_Enforcement, and Goal Oriented
+
+#We call the above the main topic list, and basically write an SEC classification function, only 
+#instead using job topics
+
+#with company description, they were generally a paragraph long, we want to filter a page or so job 
+#description, and turn it into 5-10 different paragraphs/sentences, and then judge each one accordingly
+#Figuring out how to parse the resume and turn it into a topic, or several topics is one of the main 
+#objectives for this project...but once we do this, we can compare these to the categories listed above
+
+
 
 
 Leadership_Keywords = ['haired','Controlled','Coordinated','Executed','Headed','Operated','Orchestrated',\
     'Organized','Oversaw','Planned','Produced','Programmed']
 
-
-
-'You Envisioned and Brought a Project to Life' 
-'And if you actually developed, created, or introduced that project into your company? Try:'
 Creation_Keywords = ['Administered','Built','Charted','Created','Designed','Developed','Devised',\
     'Founded', 'Engineered','Established', 'Formalized','Formed','Formulated','Implemented',\
         'Incorporated','Initiated','Instituted','Introduced','Launched','Pioneered','Spearheaded']
-print(Creation_Keywords)
 
-
-'You Saved the Company Time or Money'
-'Hiring managers love candidates who’ve helped a team operate more efficiently or cost-effectively. To show just how much you saved, try:'
 Efficiency_Keywords = ['Conserved','Consolidated','Decreased','Deducted','Diagnosed','Lessened',\
     'Reconciled','Reduced','Yielded']
 
 
-
-'You Increased Efficiency, Sales, Revenue, or Customer Satisfaction'
-'Along similar lines, if you can show that your work boosted the company’s numbers in some way, you’re bound to impress. In these cases, consider:'
 Growth_Boosting_Keywords = ['Accelerated','Achieved','Advanced','Amplified','Boosted','Capitalized',\
     'Delivered','Enhanced','Expanded','Expedited','Furthered','Gained','Generated','Improved',\
         'Lifted','Maximized','Outpaced','Stimulated','Sustained']
 
 
-
-'You Changed or Improved Something'
-'So, you brought your department’s invoicing system out of the Stone Age and onto the interwebs? Talk about the amazing changes you made at your office with these words:'
 Process_Improving_Keywords = ['Centralized','Clarified','Converted','Customized','Influenced',\
     'Integrated','Merged','Modified','Overhauled','Redesigned','Refined','Refocused','Rehabilitated',\
         'Remodeled','Reorganized','Replaced','Restructured','Revamped','Revitalized','Simplified',\
             'Standardized','Streamlined','Strengthened','Updated','Upgraded','Transformed']
 
 
-
-'You Managed a Team'
-'Instead of reciting your management duties, like “Led a team…” or “Managed employees…” show what an inspirational leader you were with terms like:'
 Management_Keywords = ['Aligned','Cultivated','Directed','Enabled','Facilitated','Fostered','Guided',\
     'Hired','Inspired','Mentored','Mobilized','Motivated','Recruited','Regulated','Shaped','Supervised',\
         'Taught','Trained','Unified','United']
 
 
-'You Brought in Partners, Funding, or Resources'
-'Were you “responsible for” a great new partner, sponsor, or source of funding? Try:'
 Client_Based_Keywords = ['Acquired','Forged','Navigated','Negotiated','Partnered','Secured']
 
 
-
-'You Supported Customers'
-'Because manning the phones or answering questions really means you’re advising customers and meeting their needs, use:'
 Consulting_Keywords = ['Advised','Advocated','Arbitrated','Coached','Consulted','Educated'\
     'Fielded','Informed','Resolved']
 
 
-
-'You Were a Research Machine'
-'Did your job include research, analysis, or fact-finding? Mix up your verbiage with these words:'
 Research_Analysis_Keywords = ['Analyzed','Assembled','Assessed','Audited','Calculated','Discovered',\
     'Evaluated','Examined','Explored','Forecasted','Identified','Interpreted','Investigated',\
         'Mapped','Measured','Qualified','Quantified','Surveyed','Tested','Tracked']
 
 
-
-'You Wrote or Communicated'
-'Was writing, speaking, lobbying, or otherwise communicating part of your gig? You can explain just how compelling you were with words like:'
 Writing_Communication_Keywords = ['Authored','Briefed','Campaigned','Co-authored','Composed','Conveyed',\
     'Convinced','Corresponded','Counseled','Critiqued','Defined','Documented','Edited','Illustrated',\
         'Lobbied','Persuaded','Promoted','Publicized','Reviewed']
 
 
-'You Oversaw or Regulated'
-'Whether you enforced protocol or managed your department’s requests, describe what you really did, better, with these words:'
-Management_Judgment_Keywords = ['Authorized','Blocked','Delegated','Dispatched','Enforced','Ensured',\
+Management_Enforcement_Keywords = ['Authorized','Blocked','Delegated','Dispatched','Enforced','Ensured',\
     'Inspected','Itemized','Monitored','Screened','Scrutinized','Verified']
 
 
-
-'You Achieved Something'
-'Did you hit your goals? Win a coveted department award? Don’t forget to include that on your resume, with words like:'
 Goal_Oriented_Keywords = ['Attained','Awarded','Completed','Demonstrated','Earned','Exceeded','Outperformed',\
     'Reached','Showcased','Succeeded','Surpassed','Targeted']
