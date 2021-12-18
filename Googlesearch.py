9b638f06353a5ffe7ef80a54930e590dc33faa1a
jesse berkowitz <jberkow@brandeis.edu>
2021-12-18 13:40:32 -0500

    scraper coming along

--- Googlesearch.py
+++ Googlesearch.py
@@ -5,6 +5,7 @@ import re
 import urllib.parse
 from urllib.parse import urlparse
 import time
+import json
 
 def googleSearch(query):
     g_clean = [ ] 
@@ -78,7 +79,7 @@ class Motley_Scraper():
     self.titles = ['Officer', 'Analyst', 'Relations', 'Head', 'Director', 'Chief', 'Senior', 'President']
     self.sentence_enders = [".", '!', '?', ","]
   
-  def conversation(self):
+  def create_conversation_list(self):
     'Scrapes Motley Fool and creates conversation'
 
     for x in self.list_of_links:
@@ -109,7 +110,7 @@ class Motley_Scraper():
             break
 
       text = B[start_val:end_val-1]
-      
+      #full_text creates relevant text for the particular document
       full_text = ' '.join(text)
 
       self.conversation_list.append(full_text)
@@ -131,13 +132,11 @@ class Motley_Scraper():
       while length >0:
 
         word = A[index]
-              # for word in A:
-          # index +=1
+          
         if word == '--':
           current = index
-          # print(current)
-          val = current            
-              
+          
+          val = current              
           
           for next_word in A[current:current+15]:
             
@@ -174,16 +173,12 @@ class Motley_Scraper():
 
       speakers = []
       speaker_vals = []
-      final_speeches = []     
-      
+           
 
       for x in speaker_indices:
         speakers.append(x[0])
         speaker_vals.append(x[1])
-      # print(speakers, speaker_vals)
-
-      # print(speakers)
-      # print(speaker_vals)
+      
       speaker_length = len(speakers)
 
       index = 0
@@ -196,7 +191,7 @@ class Motley_Scraper():
           next = speaker_vals[index+1]
           next -=3          
 
-          text = A[current:next]
+          text = A[current:next+1]
           
           if len(text)>0:
 
@@ -220,9 +215,7 @@ class Motley_Scraper():
                 text[-1]=replace
           
           speaker_speech = (' '.join(text))
-        self.individual_conversations.append((speakers[index], speaker_speech))
-                
-        
+        self.individual_conversations.append((speakers[index], speaker_speech, self.year))       
         
         index+=1
         speaker_length -=1
@@ -231,28 +224,167 @@ class Motley_Scraper():
           self.individual_conversations.remove(x)
       self.individual_conversations= self.individual_conversations[0:-2]
 
+
+#if Apple transcripts use Motley to scrape, pass it into the scraper
+'''
 Search = find_useful_searches('apple', 2021)
 if Search[0]== 'Motley':
   A = Motley_Scraper(Search[1], 2021)
-  A.conversation()
+  A.create_conversation_list()
+  #let's store this in a json file to operate on
   # print(A.conversation_list)
-  A.subdivide_conversations()
   
-  print(A.individual_conversations)
-
-#Example of how the format is returned: List of Tuples(Speaker:Text)
-
-# ('Zach Kirkhorn', "Financial Officer I mean we are carrying some amount of costs associated with the factories today. 
-# And so the incremental cost associated with turning the factories, it's not 100% of a factory, if that's what you're
-#  getting at in your question"),
-
-#  ('Jed Dorsheimer', "Genuity -- Analyst Yes, yes. That's what I was getting at"),
-# etc...
+  with open('Apple_transcripts.json', 'w') as f:
+      json.dump(A.conversation_list, f)
+'''
 
 
+#lets open Apple Transcripts and operate on the text
+
+#Begin here
+with open('Apple_transcripts.json') as f:
+  Transcript = json.load(f)
+
+endings = ['.', '?', '!']
+a = Transcript[0].split()
+updated_transcript = []
+#Find operator text up front, remove it possibly
+operator_indices = []
+breaks = []
+count = 0
+for x in a:
+
+  if 'Operator' in x:
+    operator_indices.append(count)
+    op_split = x.split('Operator')
+    updated_transcript.append('Operator')
+    updated_transcript.append(op_split[1])
+    count +=2
+  
+  else:
+    updated_transcript.append(x)
+    count +=1 
+
+count = 0
+for x in updated_transcript:
+  if '--' in x:
+    breaks.append(count)
+    count +=1
+  else:
+    count +=1
+
+ending_operator = []
+
+for x in operator_indices:
+  end_found = False
+  for y in breaks:
+    if end_found == True:
+      
+      break 
+    if y >x:
+      ending_operator.append(y)
+      end_found = True 
+      break
+
+actual_endings = [x-1 for x in ending_operator]
+operator_start = [x+1 for x in operator_indices]
+
+length = len(actual_endings)
+index = 0
+
+while length >0:
+  next = index+1    
+  if index < len(actual_endings)-1:
+    if actual_endings[next]-actual_endings[index]==0:
+      del actual_endings[next]
+      del operator_start[next]
+  
+  index +=1
+  length -=1 
+
+a = len(operator_start)
+index = 0
+Operator_Speech = []
+while a >0:
+  x = updated_transcript[operator_start[index]:actual_endings[index]]
+  updated_x = []
+  for val in x[:-1]:
+    updated_x.append(val)
+  aaa = x[-1]
+  for z in endings:
+    if z in aaa:
+      zz = aaa.split(z)
+      updated_x.append(zz[0])
+  x1 = ' '.join(updated_x)
+  Operator_Speech.append(x1)
+  index +=1
+  a-=1
+
+#operator_speech now gives all speech from the operator for the text
+BREAKS = []
+for x in breaks:
+  val = updated_transcript[x+1]
+  if val[0] != 'I' and val[0].isupper()==True:
+         
+       
+    BREAKS.append(x)
+
+#BREAKS is a list of actual breaks where the speaker changes
+Analyst_BREAKS = []
+for x in BREAKS:
+
+  after = updated_transcript[x:x+10]
+  if 'Analyst' in after:
+    Analyst_BREAKS.append(x)
+
+length = len(Analyst_BREAKS)
+index = 0
+
+ANALYST_BREAKS = []
+
+while length >0:
+  cur = Analyst_BREAKS[index]
+  next = Analyst_BREAKS[index+1]
+  #remove all analyst possible values from BREAKS list first
+  if cur in BREAKS:
+    BREAKS.remove(cur)
+
+  if next - cur <11:
+    ANALYST_BREAKS.append(next)
+    BREAKS.remove(next)
+    index +=2
+    length -=2
+  elif next-cur >11:
+    ANALYST_BREAKS.append(cur)
+    index +=1
+    length -=1
+
+#cleaning up Breaks 
+UPDATED_BREAKS = [x for x in BREAKS if updated_transcript[x+1] =='Chief' or updated_transcript[x+1]=='Director']
+
+#UPDATED_BREAKS represents all breaks directly before company speaker speaks
+#ANALYST_BREAKS represents all breaks directly before an analyst speaks
+for x in UPDATED_BREAKS:
+  print(updated_transcript[x-2:x])
 
 
 
 
+#So now Analyst Breaks represents every break related to an analyst
+#Breaks represents every break related to a speaker of the company
+
+
+
+
+
+
+
+
+#TODO
+#Parse rest of text, return list of speaker, title, text
+
+
+  
+    
 
 
