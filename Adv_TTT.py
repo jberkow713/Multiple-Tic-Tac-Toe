2726c546c806157812bd68097fb424f48ae7fa51
jesse berkowitz <jberkow@brandeis.edu>
2022-06-19 07:42:24 -0700

    piece positions working

--- Adv_TTT.py
+++ Adv_TTT.py
@@ -25,17 +25,17 @@ class Screen:
         self.width = width
         self.height = height
         self.color = color
+        # The View is the actual Pygame Surface
         self.view = self.build_screen()
         self.fill_screen()
             
     def build_screen(self):
 
-        VIEW = pygame.display.set_mode((self.width, self.height))
-        return VIEW
+        return pygame.display.set_mode((self.width, self.height))        
     
     def fill_screen(self):
         self.view.fill(self.color)
-
+        
 class Line:
     def __init__(self, Screen,starting, ending, color):
         # starting and ending are tuples, x/y coordinates
@@ -57,29 +57,40 @@ class Board:
         self.screen_color = screen_color
         self.screen = self.return_screen()
         self.lines = []
+        self.square_size = None
+        self.Positions = {}
         self.create_lines()
         self.draw_board()
+        
 
     def return_screen(self):
-        screen = Screen(self.Width, self.Height, self.screen_color)
-        return screen
 
+        return Screen(self.Width, self.Height, self.screen_color)
+        
     def create_lines(self):
         Buff = self.Buffer
         screen = self.screen
-        self.lines.append(Line(screen,(Buff,Buff), (self.Width-Buff,Buff), self.line_color))
-        self.lines.append(Line(screen,(Buff,self.Height-Buff), (self.Width-Buff,self.Height-Buff), self.line_color))
-        self.lines.append(Line(screen,(Buff,Buff), (Buff,self.Height-Buff), self.line_color))
-        self.lines.append(Line(screen,(self.Width-Buff,Buff), (self.Width-Buff,self.Height-Buff), self.line_color))
-
+        
         X_Gap = (self.Width - 2*Buff) / self.dimensions
         Y_Gap = (self.Height -2*Buff) / self.dimensions
+        self.square_size = X_Gap/2
 
-        for i in range(self.dimensions):
+        for i in range(self.dimensions+1):
             Vert_L = Line(screen, (Buff + i*X_Gap,Buff), (Buff + i*X_Gap, self.Height-Buff), self.line_color)
             self.lines.append(Vert_L)
             Hor_L = Line(screen, (Buff ,Buff+i*Y_Gap), (self.Width-Buff, Buff+i*Y_Gap), self.line_color)
             self.lines.append(Hor_L)
+        
+        count = 0
+        for i in range(self.dimensions):
+            y_val = Buff + i*Y_Gap +.5*Y_Gap
+            
+
+            for j in range(self.dimensions):
+                x_val = Buff+j*X_Gap + .5*X_Gap                
+                self.Positions[count]=(x_val,y_val)
+                count +=1
+        
 
     def draw_board(self):
         for x in self.lines:
@@ -87,13 +98,15 @@ class Board:
 
 pygame.init()
 clock = pygame.time.Clock()
+B = Board(13,50,1000, 1000, LINE_COLOR, BOARD_COLOR)
 
 while True:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    B = Board(9,50,BOARD_WIDTH, BOARD_HEIGHT, LINE_COLOR, BOARD_COLOR)
+    B
+    pygame.draw.circle(B.screen.view, RED, B.Positions[(B.dimensions**2+1)/2 -1], B.square_size, 5)
     
     pygame.display.set_caption("Multiplayer_Tic_Tac_Toe")
     pygame.display.flip()
