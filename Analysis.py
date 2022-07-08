af3ccc4ddb8fc151f32f38c5c0c9c1b8bcb3858b
jesse berkowitz <jberkow@brandeis.edu>
2022-07-07 21:14:26 -0700

    cleaned and ordered

--- Analysis.py
+++ Analysis.py
@@ -3,70 +3,54 @@ import pandas as pd
 from glob import glob
 
 import os
-# Need to create parser here, to put txt file into pandas df
-# Need to divide the text files into Two Categories:
-# 1) Condition
-# 2) Symptoms
-
-#Each list will represent Condition:Symptom
-# Each one will be ordered, and the models will go through at the end, classifying Conditions, and symptoms
-# Into one Dataframe
+# Create Conditions list of text lists 
+# Create Symptoms list of text lists
 
 directory = r"C:\Users\JayBeast\Multiple-Tic-Tac-Toe\training_data"
 Conditions = []
 Symptoms = []
 for filename in os.listdir(directory):
     
-    if filename.endswith(".txt"):             
-        
+    if filename.endswith(".txt"):        
         PATH = os.path.join(directory, filename)                
         df = open(PATH, "r")
         lines = df.readlines()
+        lines = [x.strip() for x in lines]                
+        
         start = False 
         # Create Condition List for each patient, Selecting History of Present Illness up to Physical Exam
         Condition = []
-        for line in lines:
-            line = line.strip()
-            
+        for line in lines:                        
             if line == "Physical Exam:":
                 Conditions.append(Condition)
                 break 
             if start == True:
                 Condition.append(line)
-
             if line == "History of Present Illness:":
                 start = True
+        
         start = False 
         # Create Symptom List for each patient, Selecting History of Present Illness up to Physical Exam
         Symptom = []
-        for line in lines:
-            line = line.strip()
-            
+        for line in lines:                        
             if line == "Major Surgical or Invasive Procedure:":
                 break 
             if start == True:
                 Symptom.append(line)
                 start = False
-
             if line == "Chief Complaint:":
                 start = True
         
-        for line in lines:
-            line = line.strip()
-            
+        for line in lines:                        
             if line == "Discharge Condition:":
                 Symptoms.append(Symptom)
                 break 
             if start == True:
                 Symptom.append(line)
-
             if line == "Discharge Diagnosis:":
                 start = True                       
                    
-        df.close()  
-
-print(len(Conditions))
-print(len(Symptoms))
+        df.close()
 
 # Now that there are two lists, one with Conditions, one with Symptoms, time for topic modeling
 # Clean data, etc
