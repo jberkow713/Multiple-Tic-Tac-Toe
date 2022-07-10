c8671a5c53d50b02aec307e59a76382f13951a3c
jesse berkowitz <jberkow@brandeis.edu>
2022-07-10 10:33:16 -0700

    complete!

--- Analysis.py
+++ Analysis.py
@@ -5,20 +5,16 @@ import json
 import os
 # Create Conditions list of text lists 
 # Create Symptoms list of text lists
-
 directory = r"C:\Users\JayBeast\Multiple-Tic-Tac-Toe\training_data"
 Conditions = []
 Symptoms = []
-for filename in os.listdir(directory):
-    
-    # Each file must have a condition and symptom, in order to be added
-    
+for filename in os.listdir(directory):    
+    # Each file must have a condition and symptom, in order to be added    
     if filename.endswith(".txt"):        
         PATH = os.path.join(directory, filename)                
         df = open(PATH, "r")
         lines = df.readlines()
-        lines = [x.strip() for x in lines]                
-        
+        lines = [x.strip() for x in lines]       
         start = False 
         # Create Condition List for each patient, Selecting History of Present Illness up to Physical Exam
         Condition = []
@@ -28,10 +24,10 @@ for filename in os.listdir(directory):
             if start == True:
                 Condition.append(line)
             if line == "History of Present Illness:":
-                start = True
-                
+                start = True                
         start = False 
         # Create Symptom List for each patient, Selecting History of Present Illness up to Physical Exam
+        # And also using Discharge Diagnosis section
         Symptom = []
         for line in lines:                        
             if line == "Major Surgical or Invasive Procedure:":
@@ -40,8 +36,7 @@ for filename in os.listdir(directory):
                 Symptom.append(line)
                 start = False
             if line == "Chief Complaint:":
-                start = True
-        
+                start = True        
         start = False 
         for line in lines:                        
             if line == "Discharge Condition:":
@@ -51,13 +46,11 @@ for filename in os.listdir(directory):
             if line == "Discharge Diagnosis:":
                 start = True                       
         # If both a condition and symptom text exist, for each individual document, 
-        # Add it to the general list
+        # Add it to the general list, this is important for comparing condition-symptoms in Analysis_2.py
         if Condition!=[] and Symptom!=[]:
             Conditions.append(Condition)
             Symptoms.append(Symptom)
-
         df.close()
-
 # Initially Saving the Data to two json files to be loaded in later for topic modeling
 with open('Conditions.json', 'w') as f:
     json.dump(Conditions , f)
